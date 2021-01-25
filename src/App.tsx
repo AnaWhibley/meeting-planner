d1dd8abece17dcd075337c77ed299dc15e826721
Ana Santana <anawhibley@gmail.com>
2021-01-25 15:05:33 +0000

    Added basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-import {Login} from "./features/login/login";
+import {Login} from "./features/login/Login";
 import {useSelector} from "react-redux";
 import {selectLoggedInUser} from "./features/login/loginSlice";
 import {BrowserRouter as Router, Redirect, Route, Switch, Link} from "react-router-dom";
 import {User} from "./services/userService";
-
-const Dashboard = () => <h3>Dashboard</h3>
-const Wizard = () => <h3>Wizard</h3>
-const Profile = () => <h3>Profile</h3>
-const Calendar = () => <h3>Calendar <Link to="/profile">Profile</Link></h3>
+import {Dashboard} from "./features/dashboard/Dashboard";
+import {Profile} from "./features/profile/Profile";
+import {Wizard} from "./features/wizard/Wizard";
+import { Calendar } from './features/calendar/Calendar';
 
 function App() {
 
@@ -32,7 +31,6 @@ function App() {
                 <PrivateRoute path='/wizard' component={<Wizard/>} />
                 <PrivateRoute path='/profile' component={<Profile/>} />
                 <PrivateRoute path='/' component={<Calendar/>}/>
-
             </Switch>
         </Router>
     );
---
+++ src/features/calendar/Calendar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export function Calendar() {
+    return (
+        <div>Calendar <Link to="/profile">Profile</Link></div>
+    );
+}
