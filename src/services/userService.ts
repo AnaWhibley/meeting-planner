c09531fdffd16f0609bf011b92518fb412aafff3
Ana Santana <anawhibley@gmail.com>
2021-01-28 17:56:00 +0000

    Added content to dashboard

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -19,9 +19,9 @@ export interface LoginResponse {
 class UserService {
     public static login(username: string, password: string): Observable<LoginResponse> {
         if(username === 'a' && password === 'a') {
-            return of({data: {name: 'a', role: Role.USER}, success: true}).pipe(delay(1000))
+            return of({data: {name: 'Jose Daniel', role: Role.USER}, success: true}).pipe(delay(1000))
         }else{
-            return of({data: {name: 'a', role: Role.ADMIN}, success: true}).pipe(delay(1000))
+            return of({data: {name: 'Jose Daniel', role: Role.ADMIN}, success: true}).pipe(delay(1000))
         }
         //return of({success: false}).pipe(delay(1000))
     }
