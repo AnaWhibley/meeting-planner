c0cccabd456fff647894843ecba6f3d0e484f4f0
Ana Santana <anawhibley@gmail.com>
2021-02-12 10:40:53 +0000

    State for planner

---
+++ src/app/planner/slice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {DateTime} from 'luxon';
+
+export interface PlannerSlice {
+    busyDates: Array<BusyState>;
+}
+
+interface BusyState {
+    id: number;
+    start: string;
+    end: string;
+    allDay: boolean;
+}
+
+const createBusyState = (): BusyState => ({
+    id: Math.random(),
+    start: DateTime.utc().toFormat('ff'),
+    end: DateTime.utc().toFormat('ff'),
+    allDay: false
+});
+
+export const slice = createSlice({
+    name: 'planner',
+    initialState: {
+        busyDates: []
+    } as PlannerSlice,
+    reducers: {
+        addBusy: ((state, action) => {
+            state.busyDates.push({
+                start: action.payload.start,
+                end: action.payload.end,
+                id: Math.random(),
+                allDay: action.payload.allDay
+            });
+        })
+    },
+});
+
+export const { addBusy } = slice.actions;
+
+
+export default slice.reducer;
