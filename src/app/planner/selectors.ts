37c43520df531e96bec0d51662b49a7585541146
Ana Santana <anawhibley@gmail.com>
2021-02-15 18:55:38 +0000

    Styling calendar and added popover and mock data

---
+++ src/app/planner/selectors.ts
@@ -0,0 +1,39 @@
+import {RootState} from '../store';
+import {BusyState} from './slice';
+import {DateTime} from 'luxon';
+import {DATE_TIME_FORMAT} from '../eventCreator/slice';
+
+export const selectBusyDatesCurrentUser = (state: RootState) => state.planner.busyDatesCurrentUser.map((date: BusyState) => {
+    return {
+        ...date,
+        start: DateTime.fromFormat(date.start, DATE_TIME_FORMAT).toJSDate(),
+        end: DateTime.fromFormat(date.end, DATE_TIME_FORMAT).toJSDate(),
+        title: 'Estás ocupado',
+        canDelete: true
+    }
+});
+export const selectBusyDatesOtherUsers = (state: RootState) => state.planner.busyDatesOtherUsers.flatMap((user, index) => {
+    return user.busyDates.map((date: BusyState) => {
+        return {
+            ...date,
+            start: DateTime.fromFormat(date.start, DATE_TIME_FORMAT).toJSDate(),
+            end: DateTime.fromFormat(date.end, DATE_TIME_FORMAT).toJSDate(),
+            color: colors[index % colors.length],
+            title: user.userId + ' está ocupado',
+            canDelete: false,
+        }
+    });
+});
+
+const colors = [
+    '#81b393',
+    '#F2A49D',
+    '#B8AFE7',
+    '#86B8D4',
+    '#DFE597',
+    '#FFDAB7',
+    '#FBC0CB',
+    '#F69090',
+    '#C8B4DD',
+    '#B4EBE7'
+];
