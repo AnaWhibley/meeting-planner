435e542dbfedfe71a5cafe02417ce56e600458f3
Ana Santana <anawhibley@gmail.com>
2021-02-16 16:57:04 +0000

    Delete busy date reducer and fixed text content for events

--- src/app/planner/slice.ts
+++ src/app/planner/slice.ts
@@ -6,7 +6,7 @@ export interface PlannerSlice {
 }
 
 export interface BusyState {
-    id: number;
+    id: string;
     start: string;
     end: string;
     allDay: boolean;
@@ -18,12 +18,12 @@ const busyDatesOtherUsers = [
         busyDates: [{
             start: "2021 2 17 08 00 00",
             end: "2021 2 17 15 30 00",
-            id: 0.2075701986879172,
+            id: '0.2075701986879172',
             allDay: false,
         }, {
             start: "2021 2 17 17 00 00",
             end: "2021 2 17 18 30 00",
-            id: 0.2075701986879176,
+            id: '0.2075701986879176',
             allDay: false
         }]
     }, {
@@ -31,12 +31,12 @@ const busyDatesOtherUsers = [
         busyDates: [{
             start: "2021 2 16 08 00 00",
             end: "2021 2 16 15 30 00",
-            id: 0.2075701986879155,
+            id: '0.2075701986879155',
             allDay: false
         }, {
             start: "2021 2 15 08 00 00",
             end: "2021 2 15 15 30 00",
-            id: 0.2075701986879132,
+            id: '0.2075701986879132',
             allDay: false
         }]
     },
@@ -45,7 +45,7 @@ const busyDatesOtherUsers = [
         busyDates: [{
             start: "2021 2 15 08 00 00",
             end: "2021 2 15 15 30 00",
-            id: 0.2075701986875172,
+            id: '0.2075701986875172',
             allDay: false
         }]
     },
@@ -62,14 +62,18 @@ export const slice = createSlice({
             state.busyDatesCurrentUser.push({
                 start: action.payload.start,
                 end: action.payload.end,
-                id: Math.random(),
+                id: Math.random().toString(),
                 allDay: action.payload.allDay
             });
-        })
+        }),
+        deleteBusy: ((state, action) => {
+            const index = state.busyDatesCurrentUser.findIndex((date) => date.id === action.payload);
+            if(index > -1) state.busyDatesCurrentUser.splice(index, 1);
+        }),
     },
 });
 
-export const { addBusy } = slice.actions;
+export const { addBusy, deleteBusy } = slice.actions;
 
 
 export default slice.reducer;
