49fc1a76d6b8e98d13a3db2c3189c2153cb9b56c
Ana Santana <anawhibley@gmail.com>
2021-03-07 18:16:38 +0000

    Improved and optimized search functionality

--- src/sum.ts
+++ src/sum.ts
@@ -7,35 +7,44 @@ export function sum(a: number, b: number) {
     return a + b;
 }
 
-export function search(groupedEvent: GroupedEventDto, busyDates: Array<BusyDateDto>, idFn: () => string = uuidv4): { events: Array<EventDto>, busyDates: Array<BusyDateDto>} {
+export function search(groupedEvent: GroupedEventDto, busyDates: Array<BusyDateDto>, idFn: () => string = uuidv4, user?: string): { events: Array<EventDto>, busyDates: Array<BusyDateDto>} {
     const events: Array<EventDto> = groupedEvent.events.slice();
-    const newBusyDates = busyDates.slice();
+    let newBusyDates = busyDates.slice();
     const from: DateTime = DateTime.fromFormat(groupedEvent.from, DATE_TIME_FORMAT);
     const to: DateTime = DateTime.fromFormat(groupedEvent.to, DATE_TIME_FORMAT);
     const endWorkingHour: number = 18;
     const startWorkingHour: number = 8;
     const workingMinutes: number = 30;
 
-    const busyIntervals: Map<string, Array<Interval>> = mapBusyDatesToIntervals(busyDates);
+    const busyIntervals: Map<string, Array<BusyInterval>> = mapBusyDatesToIntervals(busyDates);
 
     for(let i = 0; i < events.length; i++){
         const duration: number = events[i].duration;
         let found = false;
 
+        if(events[i].status === 'confirmed') continue;
+
+        const userIds = events[i].participants.map(p => p.email);
+
+        //Si el usuario que creó la indisponibilidad no está entre los participantes no tenemos que cambiar el evento
+        if(user && !userIds.find(id => id === user)) continue;
+
         for(let j = from; j < to; j = j.plus({minutes: 15})){
 
-            // Last possible slot
+            //Último slot posible
             const end: DateTime = j.plus({minutes: duration});
 
-            if(j.weekday === 6 || j.weekday === 7 || j.hour < startWorkingHour || (j.hour === startWorkingHour && j.minute < workingMinutes) || end.hour > endWorkingHour || (end.hour === endWorkingHour && end.minute > workingMinutes)){
+            if(j.weekday === 6 || j.weekday === 7 || j.hour < startWorkingHour ||
+                (j.hour === startWorkingHour && j.minute < workingMinutes) || end.hour > endWorkingHour ||
+                (end.hour === endWorkingHour && end.minute > workingMinutes)){
                 continue;
             }
 
-            const userIds = events[i].participants.map(p => p.email);
             const event: Interval = Interval.after(j, {minutes: duration});
-            const isValid = userIds.every((uid) => {
-                const busyIntervalsUser = busyIntervals.get(uid) || [];
-                return busyIntervalsUser.every(interval => !interval.overlaps(event))
+
+            const isValid = userIds.every((id) => {
+                const busyIntervalsUser = busyIntervals.get(id) || [];
+                return busyIntervalsUser.every(busyInterval => !busyInterval.interval.overlaps(event))
             });
 
             if(isValid){
@@ -44,31 +53,18 @@ export function search(groupedEvent: GroupedEventDto, busyDates: Array<BusyDateD
                 found = true;
 
                 userIds.forEach(id => {
-                    const busy: Array<Interval> | undefined = busyIntervals.has(id) ? busyIntervals.get(id) : [];
-                    if(busy) {
-                        busy.push(event);
-                        busyIntervals.set(id, busy);
-                    }
+                    const arrayBusyDatesMap = busyIntervals.has(id) ? busyIntervals.get(id) as Array<BusyInterval>: [];
+                    const temp = {interval: event, allDay: false, eventId: events[i].id, id: idFn()}
+                    arrayBusyDatesMap.push(temp);
+                    busyIntervals.set(id, arrayBusyDatesMap);
                 });
 
                 events[i].date = event.start.toFormat(DATE_FORMAT);
                 events[i].time = event.start.toFormat('HH:mm'); //Change this
                 events[i].status = 'pending';
 
-                userIds.forEach(id => {
-                    const busyDateEvent: BusyDto = {start: event.start.toFormat(DATE_TIME_FORMAT), end: event.end.toFormat(DATE_TIME_FORMAT), allDay: false, eventId: events[i].id, id: idFn()}
-                    const busyDatesIndex = newBusyDates.findIndex(b => b.userId === id);
+                newBusyDates = mapBusyDatesFromIntervals(busyIntervals);
 
-                    if(busyDatesIndex !== -1) {
-                        console.log("!!!!", busyDatesIndex)
-                        newBusyDates[busyDatesIndex].busy.push(busyDateEvent);
-                    }else{
-                        const arrayBusyDates: Array<BusyDto> = [];
-                        arrayBusyDates.push(busyDateEvent);
-                        const newBusyUser: BusyDateDto = {userId: id, busy: arrayBusyDates}
-                        newBusyDates.push(newBusyUser);
-                    }
-                });
                 break;
             }
 
@@ -78,9 +74,17 @@ export function search(groupedEvent: GroupedEventDto, busyDates: Array<BusyDateD
             console.log("No hemos encontrado espacio para el evento ", events[i].id);
             events[i].status = 'error';
         }
+
     }
 
     return {events, busyDates: newBusyDates}
+}
+
+interface BusyInterval {
+    interval: Interval;
+    allDay: boolean;
+    eventId?: string;
+    id: string;
 }
 
 function mapBusyDatesToIntervals(busyDates: Array<BusyDateDto>) {
@@ -89,8 +93,38 @@ function mapBusyDatesToIntervals(busyDates: Array<BusyDateDto>) {
             const intervals = current.busy.map(b => {
                 const start = DateTime.fromFormat(b.start, DATE_TIME_FORMAT);
                 const end = DateTime.fromFormat(b.end, DATE_TIME_FORMAT);
-                return Interval.fromDateTimes(start, end);
+
+                const interval: BusyInterval = {
+                    interval: Interval.fromDateTimes(start, end),
+                    allDay: b.allDay,
+                    id: b.id
+                }
+
+                if(b.eventId) interval.eventId = b.eventId;
+
+                return interval;
             });
+
             return acc.set(current.userId, intervals);
-        }, new Map<string, Array<Interval>>());
+
+        }, new Map<string, Array<BusyInterval>>());
+}
+
+function mapBusyDatesFromIntervals(map: Map<string, Array<BusyInterval>>){
+    const busyDates = [];
+    for(let entry of map) {
+        const userId: string = entry[0];
+        const busy = entry[1].map(interval => {
+            const busyDate: BusyDto = {
+                start: interval.interval.start.toFormat(DATE_TIME_FORMAT),
+                end: interval.interval.end.toFormat(DATE_TIME_FORMAT),
+                allDay: interval.allDay,
+                id: interval.id,
+            };
+            if(interval.eventId) busyDate.eventId = interval.eventId;
+            return busyDate;
+        });
+        busyDates.push({userId, busy});
+    }
+    return busyDates;
 }
