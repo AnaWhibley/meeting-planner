a6fe00bf0b559287e30bb420243b0ab591192cef
Ana Santana Medina <anawhibley@gmail.com>
2021-03-11 15:15:35 +0000

    Added new basic tests

--- src/tests/basic-2-events.test.ts
+++ src/tests/basic-2-events.test.ts
@@ -1,127 +1,63 @@
 import {search} from "../search";
+import {mockBusyDatesResult, MockedEvents, mockEvents, mockEventsResult} from "./utils";
+import {EventDto} from "../services/eventService";
+import {DateTime} from "luxon";
+import {DATE_TIME_FORMAT} from "../app/eventCreator/slice";
 
-xdescribe('Basic 2 events', () => {
+const abraham = 'abraham.rodriguez@ulpgc.es';
+const alexis = 'alexis.quesada@ulpgc.es';
+const idFn = () => '';
+const from = "2021 05 03 08 00 00";
+const to = "2021 05 31 19 00 00";
+const events: Array<MockedEvents> = [
+    {
+        participants: [abraham, alexis],
+        duration: 60
+    },
+    {
+        participants: [abraham, alexis],
+        duration: 60
+    }
+];
+
+const mockedGroupedEvent = mockEvents(events, from, to);
+
+describe('Basic 2 events', () => {
+
+    let eventsResult: Map<string, {status: string; date?: string; time?: string}>;
+    let busyDatesResult: Map<string, Array<{start: string; end: string; eventId: string}>>;
+    let busyDatesMap: Map<string, Array<{start: string; end: string;}>>;
+    let mockedEventsResult: Array<EventDto>;
 
     beforeEach(() => {
+        eventsResult = new Map();
+        busyDatesResult = new Map();
+        busyDatesMap = new Map();
     });
 
-    const basic = {
-        groupName: 'Ordinaria 20/21',
-        from: "2021 2 10 08 00 00",
-        to: "2021 3 15 19 00 00",
-        events: [
-            {
-                id: '11',
-                name: 'Defensa TFT Ana Santana',
-                participants: [
-                    {
-                        email: 'abraham.rodriguez@ulpgc.es',  //1
-                        tag: 'Presidente Tribunal Titular'
-                    },
-                    {
-                        email: 'alexis.quesada@ulpgc.es', //8
-                        tag: 'Secretario Tribunal Titular'
-                    }],
-                duration: 60,
-                status: '',
-                date: '',
-                time: ''
-            }, {
-                id: '22',
-                name: 'Defensa TFT Jose',
-                participants: [
-                    {
-                        email: 'abraham.rodriguez@ulpgc.es',  //1
-                        tag: 'Presidente Tribunal Titular'
-                    },
-                    {
-                        email: 'alexis.quesada@ulpgc.es', //8
-                        tag: 'Secretario Tribunal Titular'
-                    }],
-                duration: 60,
-                status: '',
-                date: '',
-                time: ''
-            }
-        ]
-    };
-
-
-    xit('should not overlap with 0 indisponibilidades', () => {
-        const expected = {
-            events: [
-                {
-                    id: '11',
-                    name: 'Defensa TFT Ana Santana',
-                    participants: [
-                        {
-                            email: 'abraham.rodriguez@ulpgc.es',  //1
-                            tag: 'Presidente Tribunal Titular'
-                        },
-                        {
-                            email: 'alexis.quesada@ulpgc.es', //8
-                            tag: 'Secretario Tribunal Titular'
-                        }],
-                    duration: 60,
-                    status: 'pending',
-                    date: '10-02-2021',
-                    time: '08:30'
-                }, {
-                    id: '22',
-                    name: 'Defensa TFT Jose',
-                    participants: [
-                        {
-                            email: 'abraham.rodriguez@ulpgc.es',  //1
-                            tag: 'Presidente Tribunal Titular'
-                        },
-                        {
-                            email: 'alexis.quesada@ulpgc.es', //8
-                            tag: 'Secretario Tribunal Titular'
-                        }],
-                    duration: 60,
-                    status: 'pending',
-                    date: '10-02-2021',
-                    time: '09:30'
+    it('should not overlap with 0 indisponibilidades', () => {
+        const data = [{status: 'pending', date: '03-05-2021', time: '08:30'}, {status: 'pending', date: '03-05-2021', time: '09:30'}];
+        mockedGroupedEvent.events.forEach((ev, i) => eventsResult.set(ev.id, data[i]));
+        mockedEventsResult = mockEventsResult(mockedGroupedEvent.events, eventsResult);
+        mockedGroupedEvent.events.forEach(ev => {
+            const dateTime = ev.date + ' ' + ev.time;
+            const start = DateTime.fromFormat(dateTime, 'dd-LL-yyyy HH:mm');
+            const end = start.plus({minutes: ev.duration});
+            const busyDate = {eventId: ev.id, start: start.toFormat(DATE_TIME_FORMAT), end: end.toFormat(DATE_TIME_FORMAT)};
+            ev.participants.forEach(p => {
+                const busyArray = busyDatesResult.has(p.email) ? busyDatesResult.get(p.email) : [];
+                if(busyArray) {
+                    busyArray.push(busyDate);
+                    busyDatesResult.set(p.email, busyArray);
                 }
-            ],
-            busyDates: [
-                {
-                    userId: 'abraham.rodriguez@ulpgc.es',
-                    busy: [{
-                        allDay: false,
-                        start: '2021 2 10 08 30 00',
-                        end: '2021 2 10 09 30 00',
-                        eventId: '11',
-                        id: '1'
-                    }, {
-                        allDay: false,
-                        start: '2021 2 10 09 30 00',
-                        end: '2021 2 10 10 30 00',
-                        eventId: '22',
-                        id: '1'
-                    }]
-                },
-                {
-                    userId: 'alexis.quesada@ulpgc.es',
-                    busy: [{
-                        allDay: false,
-                        start: '2021 2 10 08 30 00',
-                        end: '2021 2 10 09 30 00',
-                        eventId: '11',
-                        id: '1'
-                    }, {
-                        allDay: false,
-                        start: '2021 2 10 09 30 00',
-                        end: '2021 2 10 10 30 00',
-                        eventId: '22',
-                        id: '1'
-                    }]
-                },
-            ]
-        }
-        const response = search(basic, [], () => '1');
+            });
+        });
+        const mockedBusyDatesResult = mockBusyDatesResult([], busyDatesResult);
 
-        expect(response).toEqual(expected);
+        const result = search(mockedGroupedEvent, [], idFn);
+
+        expect(result.events).toEqual(mockedEventsResult);
+        expect(result.busyDates).toEqual(mockedBusyDatesResult);
     });
 
 });
